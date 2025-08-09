ee1997d6dc20d428ee6a3c38e839bfc5473c4a38
Nicolas Silva <araujo.silva.nicolas@gmail.com>
2025-08-09 15:21:43 -0300

    border slide animation added to the hidden paragraphs in faq section(the method of approach was changed for better performance and fluidness)

--- script/index.js
+++ script/index.js
@@ -5,7 +5,20 @@ divToggleParaSelect.forEach((elem) => {
         const paragSelect = elem.parentElement.querySelector(".list-element-p");
         const arrowSelect = elem.parentElement.querySelector(".list-element-showhide-arrow");
 
-        paragSelect.classList.toggle("hidden");
         arrowSelect.classList.toggle("rotated");
+        
+        if(paragSelect.classList.contains("expanded")) {
+            paragSelect.style.height = paragSelect.scrollHeight + 'px';
+            requestAnimationFrame(() => {
+                paragSelect.style.height = '0px';
+            });
+            paragSelect.classList.remove('expanded');
+            // paragSelect.style.paddingTop = '0px';
+        } else {
+            paragSelect.classList.add('expanded');
+            const height = paragSelect.scrollHeight + 'px';
+            paragSelect.style.height = height;
+            // paragSelect.style.paddingTop = '12px';
+        }
     });
 });
